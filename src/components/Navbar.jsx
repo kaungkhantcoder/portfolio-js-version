62eb2a9a51ca247ec3d24c22586e46013bc8e2e8
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-04-13 23:46:20 +0630

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { NAVIGATION_LINKS } from "../constants/index.jsx";
 import { motion } from "framer-motion";
+import mainbg from "../assets/computer_keyboard_books.png";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -38,7 +39,7 @@ const Navbar = () => {
       {/* Hamburger Button */}
       <button
         onClick={toggleMenu}
-        className="p-3 rounded-lg border-b-4 border-l-2 active:border-gray-900 active:shadow-none shadow-lg bg-gradient-to-tr from-gray-600 to-gray-900 border-gray-700 transition-colors focus:outline-none"
+        className="p-3 rounded-lg border-b-4 border-l-2 border-[#6c9625] active:border-[#76B900] active:shadow-none shadow-lg bg-[#76B900] transition-colors focus:outline-none"
       >
         {isMenuOpen ? <FaTimes className="h-6 w-6" /> : <FaBars className="h-6 w-6" />}
       </button>
@@ -52,16 +53,21 @@ const Navbar = () => {
           exit="exit"
           className="fixed top-16 w-[17rem] right-6 my-2 hover:text-white bg-white drop-shadow-lg rounded-xl shadow-lg p-4"
         >
-          <span className="inline-block w-[3rem] h-1 bg-gray-800 ml-1.5 my-3"></span>
+          <span className="inline-block w-[3rem] h-1 bg-[#76B900] ml-1.5 my-3"></span>
           <ul className="grid grid-cols-2 gap-4">
+            <div className="col-span-2 bg-white drop-shadow-xl rounded-md shadow-lg w-full p-[5px]">
+              <img className="w-full object-cover rounded-md h-[50px]" src={mainbg} />
+
+            </div>
+
             {NAVIGATION_LINKS.map((item, index) => (
               <li key={index}
-              className={`${index === NAVIGATION_LINKS.length - 1 ? "col-span-2" : ""}`}
+                className={`${index === NAVIGATION_LINKS.length - 1 ? "col-span-2" : ""}`}
               >
                 <a
                   href={item.href}
                   onClick={(e) => handleLinkClick(e, item.href)}
-                  className="rounded relative block items-center text-center px-2 py-2 cursor-pointer border-b-4 border-l-2 active:border-gray-600 active:shadow-none shadow-lg bg-gradient-to-tr from-gray-600 to-gray-900 border-gray-700 hover:font-bold text-white"
+                  className="rounded relative block items-center text-center px-2 py-2 cursor-pointer border-b-4 border-[#6c9625] border-l-2 active:border-[#76B900] active:shadow-none shadow-lg bg-[#76B900] hover:font-bold text-white"
                 >
                   <span class="relative">{item.label}</span>
                 </a>
